c2e8e5a79744e0b53709713fda3597cb37af1da3
Rasheeda436 <rasheedamohammed1@gmail.com>
2025-06-12 16:41:20 +0530

    Initial commit

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println(2+2);
+        System.out.println("test");
     }
 }
