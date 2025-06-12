5c74df8aecfdf50f34f9cd52b1d8dbbe8b4da1fc
Rasheeda436 <rasheedamohammed1@gmail.com>
2025-06-12 17:02:59 +0530

    Update commit

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -1,8 +1,8 @@
 package ex_01_Java_Basic;
-
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println(2+2);
         System.out.println("test");
+        System.out.println("Github test");
     }
 }
