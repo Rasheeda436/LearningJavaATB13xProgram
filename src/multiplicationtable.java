3681cea644af4613f49d8c3ebc1bc7b76e67c8eb
Rasheeda436 <rasheedamohammed1@gmail.com>
2025-07-01 10:45:05 +0530

    june 20 class exercise

--- src/multiplicationtable.java
+++ src/multiplicationtable.java
@@ -1,15 +1,21 @@
 public class multiplicationtable {
     public static void main(String[] args) {
-        System.out.println("1*9=9");
-        System.out.println("2*9=18");
-        System.out.println("3*9=27");
-        System.out.println("4*9=36");
-        System.out.println("5*9=45");
-        System.out.println("6*9=54");
-        System.out.println("7*9=63");
-        System.out.println("8*9=72");
-        System.out.println("9*9=81");
-        System.out.println("10*9=90");
+        int num = 9;
+
+        System.out.printf("%d × %d = %d", 9,1,9).println();
+        System.out.printf("%d × %d = %d", num,2, num * 2).println();
+        System.out.printf("%d × 3 = %d\n",num, num * 3);
+        System.out.printf("%d × 4 = %d\n", num, num * 4);
+        System.out.printf("%d × 5 = %d\n", num, num * 5);
+        System.out.printf("%d × 6 = %d\n", num, num * 6);
+        System.out.printf("%d × 7 = %d\n", num, num * 7);
+        System.out.printf("%d × 8 = %d\n", num, num * 8);
+        System.out.printf("%d × 9 = %d\n", num, num * 9);
+        System.out.printf("%d × 10 = %d\n", num, num * 10);
+
+
+
+
 
 
 
---
+++ src/operators/dowhileeg1.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class dowhileeg1 {
+    public static void main(String[] args)
+    {
+        int a=0;
+       /* while(a<0){
+            System.out.println(a);
+            a++;*/
+        do {
+            System.out.println(a);
+            a++;
+        }
+        while(a<0);
+        }
+    }
+//will be printed only once
